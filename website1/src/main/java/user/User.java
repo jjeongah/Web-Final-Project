0ebda1bb4be35af582ab95f02af7db9cc345a22b
jjeongah <j_eah@naver.com>
2021-12-01 01:38:38 +0900

    login mysql-eclipse connection
    a little bit error happens

--- website1/src/main/java/user/User.java
+++ website1/src/main/java/user/User.java
@@ -1,7 +1,7 @@
 package user;
 
 public class User {
-	private String userName;
+	/*private String userName;
 	private String password;
 	private String phoneNumber;
 	public String getUserName() {
@@ -21,6 +21,22 @@ public class User {
 	}
 	public void setPhoneNumber(String phoneNumber) {
 		this.phoneNumber = phoneNumber;
+	}*/
+	private String userID;
+	private String userPassword;
+	
+	public String getUserID() {
+		return userID;
 	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+
 	
 }
