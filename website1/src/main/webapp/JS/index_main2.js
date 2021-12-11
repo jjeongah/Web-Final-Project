2e31186bc86b2bb3c80c7906a64180c66537cdce
jjeongah <j_eah@naver.com>
2021-12-11 14:22:02 +0900

    add img on main page

--- website1/src/main/webapp/JS/index_main2.js
+++ website1/src/main/webapp/JS/index_main2.js
@@ -6,3 +6,19 @@ function tologinpage(){
 function tojoinpage(){
   location.href = "join.jsp";
 }
+
+let num = 0;
+function changeimg(){
+	console.log(num);
+	if(num%3== 0){
+    	document.getElementById("cafeimg").src = "./img/cafe2.jpg";
+	}
+	else if(num%3 == 1){
+		document.getElementById("cafeimg").src = "./img/cafe3.jpg";
+	}
+	else if(num%3 == 2){
+		document.getElementById("cafeimg").src = "./img/cafe.jpg";
+	}
+	num++;
+}
+
